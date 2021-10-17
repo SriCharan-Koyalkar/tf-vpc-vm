67c2c5f605bc38efaab11f484a2de93a8cf77b0f
SriCharan-Koyalkar <92539071+SriCharan-Koyalkar@users.noreply.github.com>
2021-10-17 12:45:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "project" {
   description = "ID of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
-  default       = "gcp-service-323614"
+  default       = " "
 }
 
 variable "prefix" {
@@ -33,18 +33,4 @@ variable "subnet_prefix" {
 variable "machine_type" {
   description = "Specifies the GCP instance type."
   default     = "f1-micro"
-}
-variable "height" {
-  default     = "400"
-  description = "Image height in pixels."
-}
-
-variable "width" {
-  default     = "600"
-  description = "Image width in pixels."
-}
-
-variable "placeholder" {
-  default     = "placekitten.com"
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
